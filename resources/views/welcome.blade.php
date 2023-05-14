4fad981e8ceca5359d966133f1b163cf34f0fd4f
Ruddy Gonzalo Quispe Huanca <ruddygonzqh@gmail.com>
2023-05-14 13:25:33 -0400

    Reubicacion assets, integracion icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Bienvenida | Sastreria Maya</title>
+        <link rel="shortcut icon" href="{{ asset('images/logo-icon.jpg') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
