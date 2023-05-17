c95af575be4afe4c45db06f1e15a899ca7717392
Gonzalo Q.H <ruddygonzqh@gmail.com>
2023-05-17 19:57:52 -0400

    commit

--- resources/views/usuarios/usuarios/index.blade.php
+++ resources/views/usuarios/usuarios/index.blade.php
@@ -1,5 +1,26 @@
-<ul>
-    @foreach($usuarios as $usuario)
-    <li>{{ $usuario->username}}</li>
-    @endforeach
-</ul>
+<h2> Gestión de usuarios</h2>
+
+<h3>Lista de usuarios:</h3>
+
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nombre de usuario</th>
+            <th>Correo</th>
+            <th>Rol</th>
+            <th>Opciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($usuarios as $usuario)
+        <tr>
+            <td>{{ $usuario->id }}</td>
+            <td>{{ $usuario->username }}</td>
+            <td>{{ $usuario->email }}</td>
+            <td>{{ $usuario->rol->nombre }}</td>
+            <td> Editar | Dar de baja</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
