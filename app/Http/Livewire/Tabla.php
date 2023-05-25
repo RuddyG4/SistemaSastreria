5af7dc3e64b1534258917d9ab69b4cfb3ccd8316
ArmandoPa12 <said201133@gmail.com>
2023-05-24 21:53:29 -0400

    added create,delete,view

--- app/Http/Livewire/Tabla.php
+++ app/Http/Livewire/Tabla.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Http\Livewire;
+
+use App\Models\usuarios\Funcionalidad;
 use App\Models\usuarios\Rol;
+
 
 
 use Livewire\Component;
@@ -9,17 +12,65 @@ use Livewire\Component;
 class Tabla extends Component
 {
 
+    public $funcion;
+
     public $nombre;
     public $descripcion;
+    public $tipoPermiso =['ver','crear','modificar','eliminar'];
+    public $permisos;
+    public $checkBox = [];
     public $idEdit;
-    public $afuera = 45;
+    public $afuera,$tipo,$permiso = [];
     public $show = false;
     public $showEdit = false;
     public $showDelete = false;
     public $showNew = false;
     public $contenido = '';
 
-    
+    // convierte a json
+    public function ConverJson($array)
+    {
+        foreach ($array as $permiso) {
+            [$objeto, $accion] = explode('.', $permiso);
+            
+            if (!isset($datosJson[$objeto])) {
+                $datosJson[$objeto] = [];
+            }
+            
+            $datosJson[$objeto][] = $accion;
+        }
+        return $datosJson;
+    }
+
+    // parte el array con datos como pedido.ver, cliente.ver en un array pedido, cliente
+    public function partirString($array)
+    {
+        $array1 = [];
+        foreach ($array as $permiso) {
+        $segmentos = explode('.', $permiso);
+        $array1[] = $segmentos[0];
+    }
+        return $array1 = array_unique($array1);
+    }
+    // parte los permisos de cada uno y los que se repiten apartir del .
+    public function partirPermisos($array)
+    {
+        return array_map(function($permiso) {
+            return explode('.', $permiso)[1];
+        }, $array);
+    }
+    // obtiene los id de las funcionalidades
+    public function idFuncionalidad(){
+        $idNombre = Funcionalidad::get();
+
+
+        foreach ($idNombre as $id) {
+            if (in_array($id->nombre, $this->checkBox)) {
+                $array3[] = $id->id;
+            }
+        }
+        return $array3;
+    }
     // cierra el model
     public function showoff()
     {
@@ -27,26 +78,33 @@ class Tabla extends Component
         $this->showEdit = false;
         $this->showDelete = false;
         $this->showNew = false;
+        $this->checkBox = [];
+        $this->afuera = [];
     }
 
     // funciones para crear
     public function showModel()
-    {
+    {   
+        $partir = Funcionalidad::pluck('nombre');
+        $this->permisos = $this->partirString($partir);
+
         $this->nombre =  '';
         $this->descripcion = '';
         $this->contenido = 'Crear Rol';
-        $this->show = true;
+        
         $this->showNew = true;
     }
     public function savedato()
     {
+        $rol = Rol::create([
+            'nombre' => $this->nombre,
+            'descripcion' => $this->descripcion
+        ]);
+        $funcionId = $this->idFuncionalidad();
+        $rol->funcionalidades()->attach($funcionId);
 
-        $rol = new Rol;
-        $rol->nombre =  $this->nombre;
-        $rol->descripcion =  $this->descripcion;
-        $rol->save();
 
-        $this->show = false;
+        $this->checkBox = [];
         $this->showNew = false;
         $this->clearForm();
     }
@@ -57,11 +115,19 @@ class Tabla extends Component
     {
 
         $rol = Rol::findOrFail($id);
-        $this->nombre = $rol->nombre;
-        $this->descripcion = $rol->descripcion;
-        $this->idEdit = $id;
+        $nuevo = $rol->funcionalidades()->select('permiso_rol.*')->get();
+        foreach($nuevo as $n){
+            if ($n->id_rol == $id){
+                $permisosId[] = $n->id_funcionalidad;
+            }
+        }
+        $permisosList = Funcionalidad::whereIn('id', $permisosId)->pluck('nombre');
+        foreach($permisosList as $permisos){
+           $convertir[] = $permisos;
+        }
+        $this->tipo = $this->ConverJson($convertir);
+
         $this->contenido = "Editar Rol";
-        $this->show = true;
         $this->showEdit = true;
     }
 
@@ -79,20 +145,39 @@ class Tabla extends Component
 
     // funciones para eliminar
     public function deleteRol($id)
-    {   $this->idEdit = $id;
+    {   
+        $this->idEdit = $id;
         $this->contenido = 'Elimiar Rol';
-        $this->show = true;
         $this->showDelete= true;
     }
 
     public function Delete()
     {
         $rol = Rol::find($this->idEdit);
-            if ($rol) {
-            $rol->delete();
+        if ($rol) {
+            $rol->funcionalidades()->detach();
+            $rol->delete($this->idEdit);
         }
-        $this->show = false;
         $this->showDelete = false;
+    }
+
+    // funcion para ver los datos
+    public function ver($id)
+    {   
+        $rol = Rol::findOrFail($id);
+        $nuevo = $rol->funcionalidades()->select('permiso_rol.*')->get();
+        foreach($nuevo as $n){
+            if ($n->id_rol == $id){
+                $permisosId[] = $n->id_funcionalidad;
+            }
+        }
+        $permisosList = Funcionalidad::whereIn('id', $permisosId)->pluck('nombre');
+        foreach($permisosList as $permisos){
+           $convertir[] = $permisos;
+        }
+        $this->tipo = $this->ConverJson($convertir);
+        $this->show = true;
+        $this->contenido = "Ver Permisos";
     }
 
     public function clearForm()
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,6 +8,10 @@
         <link rel="shortcut icon" href="{{ asset('images/logo-icon.jpg') }}">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <link rel="shortcut icon" href="{{ asset('images/logo-icon.jpg') }}">
+
+        <link rel="stylesheet" href="{{ asset('css/') }}">
+
+
     </head>
     <body>
         <nav class="navbar bg-body-tertiary">
@@ -28,6 +32,11 @@
                         </div>
                       </div>  
         </nav>
+
+        <button type="button" class="btn btn-primary">
+          <i class="fa-pencil" style="backgroundcolor: red"></i> Botón con Icono
+        </button>
+        
         
         {{-- <div class="container-fluid">
             <div class="row">
