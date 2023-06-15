e5d8965ba531e3433a0c06cd953aff26e391fd6a
ArmandoPa12 <said201133@gmail.com>
2023-06-15 15:39:20 -0400

    fixed

--- app/Http/Livewire/Inventario/Materiales.php
+++ app/Http/Livewire/Inventario/Materiales.php
@@ -4,49 +4,46 @@ namespace App\Http\Livewire\Inventario;
 
 use App\Models\inventario\Almacen;
 use App\Models\inventario\Material;
+use App\Models\servicios\Medida_Material;
 use Livewire\Component;
 
 class Materiales extends Component
 {
-    public $idMaterial, $unidad, $nombre, $almacen, $cantidad;
+    public $idMaterial, $nombre, $medida, $mensaje;
+
+    public $idMedida, $nombreMedida;
     public $example;
-    PUBLIC $listAlma, $listMaterial, $listPivot;
+    PUBLIC $listMedida, $listMaterial, $listPivot,$medidas;
     public function render()
     {
         // query para obtener la lista de almacenes para el modal :crear:   
-        $this->listAlma = Almacen::get();
+        $this->listMedida = Medida_Material::where('activo', 0)->get();
 
-        // query para la tabla  
-        $this->listMaterial = Material::join('inventario', 'material.id', '=', 'inventario.id_material')
-        ->join('almacen', 'inventario.id_almacen', '=', 'almacen.id')
-        ->selectRaw('almacen.*, almacen.nombre as nombreAlma, material.*, inventario.*')
-        ->get();
-
+        $this->listMaterial = Material::join('medida_material', 'material.id_medida', '=', 'medida_material.id')
+                            ->select('material.*', 'medida_material.tipo_medida')
+                            ->get();
         return view('livewire.inventario.materiales');
     }
 
     protected $rules = [
         'nombre' => 'required|',
-        'unidad' => 'required|string',
-        'cantidad' => 'required|numeric',
+        'unidad' => 'required|',
     ];
 
     protected $messages = [
         'nombre.required' => 'El nombre es obligatorio',
         'unidad.required' => 'La unidad es obligatoria',
-        'cantidad.required' => 'La cantidad es obligatoria',
-        'unidad.string' => 'La unidad tiene que contener solo letras',
-        'cantidad.numeric' => 'La cantidad tiene que ser un numero',
     ];
     public function store()
     {   
-        $this->validate();
-        $material = Material::create([
-            'nombre' => $this->nombre,
-            'tipo_unidad' => $this->unidad
+        $this->validate([
+            'nombre' => 'required',
+            'medida' => 'required',
         ]);
-        $almacen_id = $this->almacen;
-        $material->almacen()->attach($almacen_id, ['cantidad' => $this->cantidad]);
+        Material::create([
+            'nombre' => $this->nombre,
+            'id_medida' => $this->medida
+        ]);
 
         $this->cerrar();
     }   
@@ -55,7 +52,6 @@ class Materiales extends Component
     {
         $material = Material::find($this->idMaterial);
         if ($material) {
-            $material->almacen()->detach();
             $material->delete($this->idMaterial);
         }
         $this->cerrar();
@@ -66,48 +62,94 @@ class Materiales extends Component
         $this->idMaterial = $materialID;
         $material = Material::findOrFail($materialID);
         $this->nombre = $material->nombre;
-        $this->unidad = $material->tipo_unidad;
-        $Pivot = Almacen::join('inventario', 'almacen.id', '=', 'inventario.id_almacen')
-        ->select('almacen.nombre', 'inventario.cantidad','almacen.id')
-        ->where('inventario.id_material', $materialID)
-        ->get();
-
-        foreach ($Pivot as $p){
-            $this->cantidad = $p->cantidad;
-            $this->almacen = $p->id;
-        }
-
+        $this->medida = $material->id_medida;
     }
 
     public function update()
     {
         $this->validate([
-            'nombre' => 'required|',
-            'unidad' => 'required|string',
-            'cantidad' => 'required|numeric',
+            'nombre' => 'required',
+            'medida' => 'required',
         ]);
         $material = Material::findOrFail($this->idMaterial);
         $material->nombre = $this->nombre;
-        $material->tipo_unidad = $this->unidad;
-        if ($material) {
-            $material->almacen()->sync([
-                $this->almacen => [
-                    'cantidad' => $this->cantidad
-                ]
-            ]);
-        }
+        $material->id_medida = $this->medida;
+        
         $material->push();
         $this->cerrar();
     }
 
+    public function mount()
+    {
+        $this->medida = '1';
+    }
     public function cerrar()
     {    
-        $this->reset(['idMaterial', 'unidad', 'nombre','almacen','cantidad']);
+        $this->reset(['idMaterial', 'nombre','medida']);
         $this->dispatchBrowserEvent('cerrar-modal-crear');
         $this->dispatchBrowserEvent('cerrar-modal-eliminar');
         $this->dispatchBrowserEvent('cerrar-modal-editar');
         $this->resetErrorBag();
 
+    }
+
+// funciones de los modales de medidas
+    public function storeMedida()
+    {
+        $this->validate([
+            'nombreMedida' => 'required',
+        ]);
+
+        $registro = Medida_Material::where('tipo_medida', $this->nombreMedida)->pluck('id');
+
+        if ($registro->isNotEmpty()) {
+            $idExistente = $registro[0];
+            $medida= Medida_Material::findOrFail($idExistente);
+            $medida->activo = 0;
+            $medida->push();
+        } else {
+            Medida_Material::create([
+                'tipo_medida' => $this->nombreMedida,
+            ]);
+        }
+
+
+
+
+        // $nombreExistente = Medida_Material::where('tipo_medida', $this->nombreMedida)->exists();
+       
+        // if ($nombreExistente)
+        // {
+        //     $medida= Medida_Material::findOrFail($nombreExistente->id);
+        //     $medida->activo = 1;
+        // }else
+        // {
+        //     Medida_Material::create([
+        //         'tipo_medida' => $this->nombreMedida,
+        //     ]);
+        // }
+        
+
+        $this->cerrarMedida();
+    }
+
+    public function DarBaja()
+    {
+        $medida= Medida_Material::findOrFail($this->idMedida);
+        $medida->activo = 1;
+        
+        $medida->push();
+        $this->cerrarMedida();
+    }
+
+
+    public function cerrarMedida()
+    {   
+        $this->reset(['idMedida', 'nombreMedida']);
+        $this->dispatchBrowserEvent('cerrar-modal-medida');
+        $this->dispatchBrowserEvent('cerrar-modal-crear-medida');
+        $this->dispatchBrowserEvent('cerrar-modal-baja-medida');
+        $this->resetErrorBag();
     }
 
 }
